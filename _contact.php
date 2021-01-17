14dcae90e391563f884f945d6bd60951a043442a
CHANKISANGRAWAL <66659739+chankisangrawal@users.noreply.github.com>
2021-01-17 16:48:40 +0530

    aeroclub website

---
+++ _contact.php
@@ -0,0 +1,23 @@
+<section id="methodology" class="container mt-5">
+
+ <div class="row">
+   <div class="col-sm-5">
+     <img src="assest/deepika.jpg" class="img-fluid " alt="">
+   </div>
+   <div class="col-sm-5">
+   <div class="card text-center bg-info " style="box-shadow: 0pc 10px 12px grey;">
+    <h5> Coordinator HBTU aeroclub</h5>
+
+    <div class="card-body">
+      <h5 class="card-title">Deepika Junaja
+        3rd Btech Mechanical
+
+      </h5>
+      
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+    </div>
+  </div>
+   </div>
+ </div>
+</section>
---
+++ index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+
+
+    // event owl carousel
+    $("#event .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTime: 2000,
+        dots: false,
+        responsive: {
+            0: {
+                nav: false,
+                items: 1
+            },
+           600: {
+                nav: false,
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
+
